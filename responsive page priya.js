dc17784db4f926c8710264f466889cf97be489d3
PriyaLakshme26 <154366169+PriyaLakshme26@users.noreply.github.com>
2023-12-20 02:17:24 +0530

    Add files via upload

---
+++ responsive page priya.js
@@ -0,0 +1,24 @@
+function mouseOver1() {
+    document.getElementById("box1").style.backgroundColor = "yellow";
+
+  }
+  function mouseOut1() {
+    document.getElementById("box1").style.backgroundColor = "rgb(255, 0, 187)";
+    
+  }
+  function mouseOver2() {
+    document.getElementById("box2").style.backgroundColor = "rgb(0, 255, 166";
+
+  }
+  function mouseOut2() {
+    document.getElementById("box2").style.backgroundColor = "rgb(255,0,98)";
+    
+  }
+  function mouseOver3() {
+    document.getElementById("box3").style.backgroundColor = "rgb(0, 204, 255";
+
+  }
+  function mouseOut3() {
+    document.getElementById("box3").style.backgroundColor = "rgb(149, 0,255)";
+    
+  }
